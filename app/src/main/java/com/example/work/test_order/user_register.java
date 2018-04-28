5e60e1aa0ac73ba62efd713ef015457ba85071dc
Unknown <bfmpa_13363@hotmail.com>
2018-04-28 07:07:20 +0700

    hasmap insert not compet yet
    still not complet testintent senddata

--- app/src/main/java/com/example/work/test_order/user_register.java
+++ app/src/main/java/com/example/work/test_order/user_register.java
@@ -3,14 +3,26 @@ package com.example.work.test_order;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class user_register extends AppCompatActivity {
     EditText register_pw, register_user;
     Intent testintent;
+    final String url = "http://localhost/FoodOrder_DB/rgister_in.php";
     String text = "Tesst_intent";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +35,12 @@ public class user_register extends AppCompatActivity {
     public void onclick_regis(View view) {
         if (emptyValidate(register_pw, register_user)) {
             Toast.makeText(this, "empty", Toast.LENGTH_SHORT).show();
+            testintent.putExtra("", "");
+            setResult(RESULT_OK, testintent);
 
         } else {
             Toast.makeText(this, "Notempty", Toast.LENGTH_SHORT).show();
-            testintent.putExtra("","");
-            setResult(RESULT_OK, testintent);
-            finish();
+            register_insert();
         }
     }
 
@@ -36,5 +48,30 @@ public class user_register extends AppCompatActivity {
         String user = register_user.getText().toString();
         String password = register_pw.getText().toString();
         return (password.matches("") && user.matches(""));
+    }
+
+    public void register_insert() {
+        StringRequest request = new StringRequest(StringRequest.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("id",register_user.getText().toString());
+                params.put("password",register_pw.getText().toString());
+                Log.d("a","Inhasmap");
+                return params;
+
+            }
+        };
+        Volley.newRequestQueue(this).add(request);
     }
 }
